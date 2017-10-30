5c7fe29fd61e54fc39f78d172ae754356e11695e
IanChen6 <626614767@qq.com>
2017-10-30 18:09:33 +0800

    1
    1

--- untitled1/geshihua/fromat.py
+++ untitled1/geshihua/fromat.py
@@ -13,12 +13,16 @@ __author__ = 'IanChen'
 #     percentage = ((a - b) / a) * 100
 #     percentage = "%.2f %%" % (percentage)
 #     print("成绩下降"+percentage)
-print(hex(100))
+# print(hex(100))
+#
+# import math
+# def quadratic(a,b,c):
+#     gen =math.sqrt((b**2-4*a*c))
+#     a1=(-b+gen)/2
+#     a2=(-b - gen)/2
+#     print("该一元二次方程的两个解分别为：%.0f,%.1f"%(a1,a2))
+# quadratic(1,-2,-3)
 
-import math
-def quadratic(a,b,c):
-    gen =math.sqrt((b**2-4*a*c))
-    a1=(-b+gen)/2
-    a2=(-b - gen)/2
-    print("该一元二次方程的两个解分别为：%.0f,%.1f"%(a1,a2))
-quadratic(1,-2,-3)
+print('\x74\xf7\xd4\x7c\x06\x15\x9b\x4f\xd9\x15\x7e\xd6\x7d\x3e\x5e\xe1')
+print(b'\x04\x07\x04\x0c\x06\x05\x0b\x0f\x09\x05\x0e\x06\x0d\x0e\x0e\x01'.decode("ascii"))
+
