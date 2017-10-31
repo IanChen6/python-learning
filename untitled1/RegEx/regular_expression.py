fea998620ba0a354a741cdbc9d8455bca4080bae
IanChen6 <626614767@qq.com>
2017-10-31 18:34:51 +0800

    2
    2

--- untitled1/RegEx/regular_expression.py
+++ untitled1/RegEx/regular_expression.py
@@ -1,3 +1,19 @@
 # -*- coding:utf-8 -*-
 __author__ = 'IanChen'
 
+import re
+
+string = 'This is a\nnormal string'
+a=re.match(r'This',string)
+print(a.group())
+a.start()
+a.end()
+print(a.start())
+print(a.end())
+
+contactInfo = 'Doe,John:555-1212'
+match=re.search(r'(?P<first>\w+),(?P<second>\w+):(?P<third>.*)',contactInfo)
+print(match.group('first'))
+print(match.group('second'))
+print(match.group('third'))
+
--- untitled1/duoxiancheng/createthread.py
+++ untitled1/duoxiancheng/createthread.py
@@ -12,7 +12,8 @@ def loop():
 
 #主程序
 print('thread %s is running...'%threading.current_thread().name)
-th=threading.Thread(target=loop,name="newthread")#启动新线程，target传入目标方法，name为新线程名字，为命名则默认thread1、thread2...
-th.start()
-th.join()
-print('thread %s end...'%threading.current_thread().name)
+# th=threading.Thread(target=loop)#启动新线程，target传入目标方法，name为新线程名字，为命名则默认thread1、thread2...
+# th.start()
+# th.join()
+# print('thread %s end...'%threading.current_thread().name)
+loop()
